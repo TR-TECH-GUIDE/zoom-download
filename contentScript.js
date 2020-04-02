b8e132409accda99f1970e5cce55f57e4f2266fb
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-04-02 18:33:22 -0400

    Update style and wait for jQuery to load

--- contentScript.js
+++ contentScript.js
@@ -4,15 +4,9 @@
 function runScript(name) {
   var s = document.createElement('script');
   s.src = chrome.runtime.getURL(name);
-  // s.onload = function() {
-  //     this.remove();
-  // };
   (document.head || document.documentElement).appendChild(s);
 }
 
-// Inject JQuery and then the custom script
+// Inject jQuery and then the custom script
 runScript('jquery.js')
 runScript('injectedScript.js')
-
-// TODO: See if this is necessary
-//setTimeout(() => {runScript('injectedScript.js')}, 1000)
--- injectedScript.js
+++ injectedScript.js
@@ -1,21 +1,31 @@
 let URL = window.__data__.viewMp4Url
+
+// This should not loop forever, but will if jQuery is never loaded
+// TODO: Figure out a better way
+while (!window.jQuery) { }
+
 $(document).ready(function() {
+    // Remove the old download button, if it exists
+    $('.download').remove()
+
+    // Add the download button to the page
+    // NOTE: Indentation reduced so it inserts with proper spacing
     $('.r-header-row').append(`
-        <div id="btn">Download Meeting Video</div>
-        <div class="modal-wrapper">
-			<div class="modal">
-				<ol>
-					<li>Right click <a id="videoLink" href="javascript:void()">this link</a></li>
-					<li>Choose "Save link as..."</li>
-					<li>Your browser will now download the video to the chosen location</li>
-				</ol>
-			</div>
-		</div>
-        `)
+<div id="btn"><i class="zm-icon-download mgr-xs"></i>Download Video</div>
+<div class="modal-wrapper">
+  <div class="modal">
+    <ol>
+      <li>Right click <a id="videoLink" href="javascript:;">this link</a></li>
+      <li>Choose "Save link as..."</li>
+      <li>The video will download</li>
+    </ol>
+  </div>
+</div>
+`);
 
-
+    // Display download instructions with valid download URL
     $("#btn").click(function() {
-        console.log('' + window)
+      console.log('' + window)
       $(".modal-wrapper")
         .css("display", "flex")
         .hide()
@@ -31,4 +41,4 @@ $(document).ready(function() {
       .click(function(e) {
         return false;
       });
-  });
+});
