e45c0741e35ce747524e18285a34ae4ee55d69a1
Logan Snow <loganhsnow@gmail.com>
2020-04-02 22:59:23 -0400

    Add timeout to loop waiting for jQuery. Add close button

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
 let URL = window.__data__.viewMp4Url
-$(document).ready(function() {
 
-    $('.r-header-row').append(`
-        <div id="btn">Download Meeting Video</div>
-        <div class="modal-wrapper">
-			<div class="modal">
-				<ol>
-					<li>Right click <a id="videoLink" href="javascript:void()">this link</a></li>
-					<li>Choose "Save link as..."</li>
-					<li>Your browser will now download the video to the chosen location</li>
-				</ol>
-			</div>
-		</div>
-        `)
+function insertButton(){
+  if(!window.jQuery) {
+    setTimeout(insertButton, 100)
+  } else {
+    $(document).ready(function() {
 
-
-    $("#btn").click(function() {
-        console.log('' + window)
-      $(".modal-wrapper")
-        .css("display", "flex")
-        .hide()
-        .fadeIn();
-        $('#videoLink').attr('href', URL);
-        $('#videoLink').attr('download', window.__data__.topic);
-    });
-    $(".modal-wrapper")
-      .click(function() {
-        $(this).fadeOut();
-      })
-      .children()
-      .click(function(e) {
-        return false;
+      $('.r-header-row').append(`
+          <div id="btn">Download Meeting Video</div>
+          <div class="modal-wrapper">
+        <div class="modal">
+          <div class="close-button">X</div>
+          <ol>
+            <li>Right click <a id="videoLink" href="javascript:void()">this link</a></li>
+            <li>Choose "Save link as..."</li>
+            <li>Your browser will now download the video to the chosen location</li>
+          </ol>
+        </div>
+      </div>
+          `)
+  
+  
+      $("#btn").click(function() {
+        $(".modal-wrapper")
+          .css("display", "flex")
+          .hide()
+          .fadeIn();
+          $('#videoLink').attr('href', URL);
+          $('#videoLink').attr('download', window.__data__.topic);
       });
-  });
+      $('.close-button').click(function() {
+        $('.modal-wrapper').fadeOut();
+      })
+      $(".modal-wrapper")
+        .click(function() {
+          $(this).fadeOut();
+        })
+        .children()
+        .click(function(e) {
+          return false;
+        });
+    });
+  }
+}
+
+insertButton()
