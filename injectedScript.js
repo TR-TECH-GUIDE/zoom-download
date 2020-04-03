82bce9f2b1e8b38fd1f427fb62bf178d274a70a2
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-04-02 23:38:23 -0400

    Restore lost code and update styles
    Style updates:
    
      - Replaced modal "X" with "&times;" so it isn't too tall
      - Changed button color to match the Zoom layout colors
      - Restored download icon that got lost in the sauce somewhere

--- injectedScript.js
+++ injectedScript.js
@@ -5,22 +5,26 @@ function insertButton(){
     setTimeout(insertButton, 100)
   } else {
     $(document).ready(function() {
+      // Remove the old download button, if it exists
+      $('.download').remove()
 
+      // Add the download button to the page
+      // NOTE: Indentation reduced so it injects with proper spacing
       $('.r-header-row').append(`
-          <div id="btn">Download Meeting Video</div>
-          <div class="modal-wrapper">
-        <div class="modal">
-          <div class="close-button">X</div>
-          <ol>
-            <li>Right click <a id="videoLink" href="javascript:void()">this link</a></li>
-            <li>Choose "Save link as..."</li>
-            <li>Your browser will now download the video to the chosen location</li>
-          </ol>
-        </div>
-      </div>
-          `)
-  
-  
+<div id="btn"><i class="zm-icon-download mgr-xs"></i>Download Video</div>
+<div class="modal-wrapper">
+  <div class="modal">
+    <div class="close-button">&times;</div>
+    <ol>
+      <li>Right click <a id="videoLink" href="javascript:;">this link</a></li>
+      <li>Choose "Save link as..."</li>
+      <li>The video will download</li>
+    </ol>
+  </div>
+</div>
+`);
+
+      // Display download instructions with valid download URL (in modal)
       $("#btn").click(function() {
         $(".modal-wrapper")
           .css("display", "flex")
